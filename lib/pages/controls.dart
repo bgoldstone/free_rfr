e5804204d1c46f491a8fcd2d3f823bfe0fb9e3fe
Benjamin Goldstone <bgoldstone1@gmail.com>
2025-09-13 12:12:23 -0400

    added more tests

--- lib/pages/controls.dart
+++ lib/pages/controls.dart
@@ -127,7 +127,7 @@ class _ControlsState extends State<Controls> {
   }
 }
 
-Center noParametersForThisChannel(String parameterTypes) {
+Widget noParametersForThisChannel(String parameterTypes) {
   return Center(
     child: Text(
       "No $parameterTypes Controls for this channel",
